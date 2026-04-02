cbc23fafd615079d318c66c170e16f1e4b9887e6
Roothulhu <rafael.hernandez.chavez@proton.me>
2026-04-02 12:11:06 -0600

    chore: build

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -2189,5 +2189,25 @@ export type BarcodeType = {
     barcodeiduser: number;
     barcodecreatedat: string;
 };
+export type OpticutterStockItemType = {
+    length: string;
+    width: string;
+    count: string;
+};
+export type OpticutterRequirementItemType = {
+    length: string;
+    width: string;
+    count: string;
+};
+export type OpticutterLayoutResultType = {
+    index: number;
+    panels: any;
+    remainders: any;
+    svgImage: string | null;
+};
+export type OpticutterCutResultType = {
+    requiredStocks: any;
+    layouts: OpticutterLayoutResultType[];
+};
 export {};
 //# sourceMappingURL=types.d.ts.map
