f56f0d4558f033bfd12e419415ed4f0057e7d827
Roothulhu <rafael.hernandez.chavez@proton.me>
2026-04-03 08:59:45 -0600

    chore: build

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1819,7 +1819,7 @@ export type ChatMessageType = {
 };
 export type ChatType = {
     idchat: number;
-    chattype: "direct" | "group" | "channel" | "community_room";
+    chattype: "direct" | "group" | "channel" | "community_room" | "aura";
     chattitle: string;
     chatdescription: string | null;
     chatavatar: string;
