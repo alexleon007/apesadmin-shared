9b4899d70d56194fcbfc8911cdb21d350e38101b
Roothulhu <rafael.hernandez.chavez@proton.me>
2026-05-14 11:15:39 -0600

    build

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -2048,6 +2048,18 @@ export type ImageElementType = BaseElementType & {
     height?: number;
     fit?: "contain" | "cover" | "scale-down";
 };
+export type ImageGridElementType = {
+    type: "image_grid";
+    x?: number;
+    y?: number;
+    data: string;
+    columns?: number;
+    cellWidth?: number;
+    cellHeight?: number;
+    gap?: number;
+    visibleIf?: string;
+    showOn?: "all" | "first" | "last";
+};
 export type TextElementType = BaseElementType & TextStyleType & {
     type: "text";
     text: string;
